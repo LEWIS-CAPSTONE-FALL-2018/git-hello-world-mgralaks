7c726da9659c97c7cba59d864f47e13ea08d1799
Mitchell Gralak <mitchellgralak@lewisu.edu>
2018-09-02 08:55:32 -0500

    Finally figured this out. Added second file to gitHub

---
+++ assignment1/assignment1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+ * Author: Mitchell Gralak
+ * Date: 09/02/2018
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
